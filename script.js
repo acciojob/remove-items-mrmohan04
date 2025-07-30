e9a2f2e8ba92be2ad30d544954874e990423cd54
mrmohan04 <gugulothmohan381@gmail.com>
2025-07-30 14:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+window.onload = function() {
+  const button = document.querySelector('input[type="button"][value="Select and Remove"]');
+  const select = document.getElementById('colorSelect');
+
+  button.addEventListener('click', function() {
+    if (select.selectedIndex >= 0) {
+      select.remove(select.selectedIndex);
+    }
+  });
+};
